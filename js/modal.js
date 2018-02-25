1e1a6fd52ddd27e5ba519f1d455a42a31a3249ff
jensnti <jens.andreasson@nti.se>
2018-02-25 16:01:27 +0100

    modal fixed, loading js, just styling left

--- js/modal.js
+++ js/modal.js
@@ -1,58 +1,40 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("modal");
+var modalContent = document.getElementById("modal-content");
 
 // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
 var elements = document.getElementsByClassName("column");
 
-/*
-var modalContent = document.createElement("div");
-modalContent.id = "modal-content";
-modalContent.className = "modal-content";
-var modalSpan = document.createElement("span");
-modalSpan.className = "close";
-modalSpan.textContent = "\u00d7";
-var modalP = document.createElement("p");
-modalP.textContent = "Some text";
-*/
-var frame = document.createElement("iframe");
-frame.id = "animFrame";
-frame.className = "animFrame";
+var frame = document.getElementById("animFrame");
 
 for(var i = 0; i < elements.length; i++) {
 	elements[i].addEventListener("click", function (e) {
 		e.preventDefault();
 
-		fetch(e.path[1].dataset.url)
-		.then(function(response) { return response.json(); })
-		.then(function(json) {
-			console.log(json);
-//			modalContent.appendChild(modalSpan);
-//			modalP.textContent = json.description;
-//			modalContent.appendChild(modalP);
-			frame.src = json.target;
-//			modalContent.appendChild(frame);
-			modal.appendChild(frame);
+		if(e.path[1].dataset.url) {
+			fetch(e.path[1].dataset.url)
+			.then(function(response) { return response.json(); })
+			.then(function(json) {
+				console.log(json);
+				frame.src = json.target;
 
-		});
+			});
 
-		modal.style.display = "block";
+			modal.style.display = "block";
+		}
 	}, true);
 }
 
 // When the user clicks on <span> (x), close the modal
-frame.contentDocument.getElementsByClassName("close").addEventListener("click", function() {
+span[0].addEventListener("click", function() {
     modal.style.display = "none";
-    while (modal.firstChild) {
-	    modal.removeChild(modal.firstChild);
-	}
 }, true);
 
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener("click", function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
-	    while (modal.firstChild) {
-		    modal.removeChild(modal.firstChild);
-		}
     }
 }, true);
